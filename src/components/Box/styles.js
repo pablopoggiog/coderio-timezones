64563a06d1d1fcefd55b8a80283cf1cd85f3ce11
Pablo Poggio <pablopoggiog@gmail.com>
2021-03-20 00:16:37 -0300

    Pollish Box styles

--- src/components/Box/styles.js
+++ src/components/Box/styles.js
@@ -1,4 +1,5 @@
 import styled, { keyframes } from "styled-components";
+import dots from "../../assets/dots.jpg";
 
 const fadeIn = keyframes`
   from {
@@ -13,23 +14,39 @@ const fadeIn = keyframes`
 export const Container = styled.div`
   height: 200px;
   width: 100%;
-  background-color: lightgrey;
+  background-image: url(${dots});
   border-radius: 10px;
+  border: 1px dotted darkgrey;
   animation: ${fadeIn} 0.6s ease 1;
+  transition: 0.6s;
 
   @media all and (min-width: 620px) {
     min-width: 200px;
     width: 30%;
   }
+
+  &:hover {
+    transform: scale(1.01);
+  }
 `;
 export const DeleteBox = styled.div`
-  width: 10px;
-  margin: 15px 20px auto auto;
+  width: 20px;
+  height: 20px;
+  margin-left: auto;
+  padding: 0.7em;
+  cursor: pointer;
+  transition: 0.6s;
+  border-radius: 10px;
+  font-weight: 600;
+
+  &:hover {
+    transform: scale(1.1);
+  }
 `;
 
 export const Content = styled.div``;
 
-export const Zone = styled.h3``;
+export const Zone = styled.h4``;
 
 export const Date = styled.p``;
 
