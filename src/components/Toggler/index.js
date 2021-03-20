274d8686a5075f1c8761704ba0d38fc3a4ddfb48
Pablo Poggio <pablopoggiog@gmail.com>
2021-03-20 14:13:08 -0300

    Updates

--- src/components/Toggler/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Container, Button } from "./styles";
-
-export const Toggler = ({ toggleTheme }) => (
-  <Container>
-    <Button onClick={toggleTheme}>Switch Theme</Button>
-  </Container>
-);
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./SearchBar";
-export * from "./Box";
-export * from "./Boxes";
-export * from "./Toggler";
